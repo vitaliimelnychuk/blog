f7875b5206e49ef9b24d24b8f6c4defe27c01d7a
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2019-12-17 12:11:29 +0200

    use Layout Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-const Index = () => (
-  <div>
-    <p>My personal blog</p>
-    <p>Deployed by now deployment</p>
-    <p>2019 ©</p>
-  </div>
+import Layout from '../components/Layout'
+
+const Home = () => (
+  <Layout >
+    <div>
+      <p>My personal blog</p>
+      <p>Deployed by now deployment</p>
+      <p>2019 ©</p>
+    </div>
+  </Layout>
 );
 
-export default Index;
+export default Home;
