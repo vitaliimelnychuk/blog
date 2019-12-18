f9c4f1e17da3d74352c07823da08c7078ab07c41
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2019-12-18 14:21:05 +0200

    add button link

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,7 @@
 
+import Button from './Button';
+import Link from 'next/link'
+
 interface IPostProps {
   title: string;
   description: string;
@@ -9,7 +12,13 @@ function Post({ title, description, url }: IPostProps) {
   return <div className="container">
     <h2>{title}</h2>
     <p className="description">{description}</p>
-    <p><a href={url}>Open</a></p>
+    <div>
+      <Link href={url}>
+        <a>
+          <Button >Read</Button>
+        </a>
+      </Link>
+    </div>
     <style jsx>{`
     .container {
         display: grid;
@@ -26,7 +35,7 @@ function Post({ title, description, url }: IPostProps) {
         line-height: 2em;
       }
     `}</style>
-  </div>;
+  </div >;
 }
 
 export default Post;
