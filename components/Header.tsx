475dda3530eb4f802d05dbf32d1a5d1f90c92e22
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2019-12-21 00:24:29 +0200

    add Header links

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,23 +2,17 @@ import Link from 'next/link';
 
 const Header = () => (
   <div className='container'>
-    <div className="logo"></div>
-    <div className="links">
-      {/* <Link href="/">
+    <div>
+      <b>Vitalii Melnychuk</b> |
+      <Link href="/">
         <a>Home</a>
-      </Link> */}
+      </Link>
     </div>
     <style jsx>{`
     .container {
         display: grid;
-        grid-auto-rows: 80px;
-        grid-template-rows: auto;
-        grid-template-columns: 1fr 1fr;
-        grid-gap: 0px;
-        justify-items: center;
-        align-items: center;
+        grid-template-columns: 1fr;
         padding: 10px;
-        border-bottom: 1px solid black;
       }
       .links a {
         font-size: 16px;
