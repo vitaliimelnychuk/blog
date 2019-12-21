53f1b26188358d9606bbb8452f6c390f509a2eba
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2019-12-21 13:46:09 +0200

    change how to deply blog title

--- content/articles.ts
+++ content/articles.ts
@@ -7,7 +7,7 @@ export type Article = {
 
 export default {
   '22-12-2019-deploy-your-blog': {
-    title: 'Deploy your blog',
+    title: 'How to deploy your own blog in 2019',
     date: new Date('22 Dec 2019'),
     url: '/articles/22-12-2019-deploy-your-blog'
   } as Article
