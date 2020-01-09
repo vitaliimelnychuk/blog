f7a275623060b2f1fd11b793cb8c9d4549e6060a
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-01-09 22:45:01 +0200

    fix grammar

--- pages/articles/2019-12-22-deploy-your-blog.tsx
+++ pages/articles/2019-12-22-deploy-your-blog.tsx
@@ -9,24 +9,24 @@ export default () => (
   <Layout >
     <ArticlePage {...article}>
       <div>
-        <p>You couldn't be sure when would be right time start your blog,
-        what's tools should be used and what kind of articles you really want to write and finally is it worth my time?</p>
+        <p>You cannot be sure when it is a right time start your blog.
+        Which tools should be used, what kinds of articles you want to write and finally is it worth your time?</p>
 
-        <p>I like blogs, especially tech blogs, they all bring me new ideas and inspiration but all the time I had one think in mind, will I be able to write something.</p>
+        <p>I like blogs, especially tech blogs. They give me new ideas and inspiration, but at the same time they make me think: "Will I be able to write something like this?".</p>
 
-        <p>But now, at the end of 2019 I'd like to present my blog and technologies have been chosen to make it happen.</p>
+        <p>However now, at the end of 2019, I'd like to present my blog and technologies that I've chosen to make it happen.</p>
 
         <h2>Overview</h2>
-        <p>I likely trying new things and prefer simple solutions in order to make support easier.</p>
-        <p>In my experience, I had a chance to work with <b>React</b> and particularly <b>Next.js</b>.
-          I like those tools and happy with them. I really like the idea to have single page application and get rid of annoying loaders. actually, what I like more it's a code splitting feature.
-          Next.js does automatic code splitting and it is based on the pages in your app. And it's mostly all I want</p>
-        <p>If you have found 1 million ways how deploy/manage you blog it wouldn't be enough, there is one more way to do it. Here is one of those ways.</p>
+        <p>I am always trying to keep up with new tech solutions. My favorite ones are those that make our life easier.</p>
+        <p>In my experience, I had a chance to work with <b>React</b> and particularly with <b>Next.js</b>.
+          I really like the idea of having a single page application and get rid of annoying loaders. Even better is having SPA and code splitting feature.
+          Next.js does automatic code splitting and it is based on the pages in your app. It's mostly what I wanted.</p>
+        <p>There are millions of ways to deploy/manage your blog. In this article, I will tell about the one I have chosen.</p>
 
-        <h2>Expectations</h2>
-        <p>As I sad before, there is few points should have done to make blog easy to maintain and flexible to changes</p>
+        <h2>Blog Expectations</h2>
+        <p>As I said before, there are few points that should be done to make blog easy-to-maintain and flexible to changes.</p>
         <ul>
-          <li>Code as source of truth</li>
+          <li>Code as a source of truth</li>
           <li>Single page application</li>
           <li>Server side rendering</li>
           <li>Code splitting</li>
