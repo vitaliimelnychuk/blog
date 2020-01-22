b3cf7f377d8eba474ac1d380b9e6928113067d04
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-01-22 22:22:47 +0200

    Add deploy your blog rest of article (#3)
    * refactor headers
    
    * add syntax highlighting
    
    * add conclusion
    
    * fix grammar

--- pages/articles/2019-12-22-deploy-your-blog.tsx
+++ pages/articles/2019-12-22-deploy-your-blog.tsx
@@ -23,6 +23,52 @@ charset = utf-8
 indent_style = space
 indent_size = 2
 `;
+
+const prismjs = `
+import React, { PureComponent } from "react"
+
+import { PropTypes } from "prop-types"
+
+export default class PrismCode extends PureComponent {
+  static propTypes = {
+    async: PropTypes.bool,
+    className: PropTypes.string,
+    children: PropTypes.any,
+    component: PropTypes.node,
+  }
+
+  static defaultProps = {
+    component: \`code\`,
+  }
+
+  componentDidMount() {
+    this._hightlight()
+  }
+
+  componentDidUpdate() {
+    this._hightlight()
+  }
+
+  _hightlight() {
+    Prism.highlightElement(this._domNode, this.props.async)
+  }
+
+  _handleRefMount = domNode => {
+    this._domNode = domNode
+  }
+
+  render() {
+    const { className, component: Wrapper, children } = this.props
+
+    return (
+      <Wrapper ref={this._handleRefMount} className={className}>
+        {children}
+      </Wrapper>
+    )
+  }
+}
+`;
+
 export default () => (
   <Layout >
     <ArticlePage {...article}>
@@ -50,7 +96,7 @@ export default () => (
           <li>Code splitting</li>
           <li>Easy deployment</li>
         </ul>
-        <h3>Starting from code</h3>
+        <h2>Starting from code</h2>
         <p>
           I'm that type of engineers who just like writing the code. In a nutshell, let's start from React, Next.js.
         </p>
@@ -66,11 +112,51 @@ export default () => (
           I consider them as more production solutions.
           We definitely need them in our more complex apps where it's so important to have knowledge of users, database operations, payments and so on.
         </p>
+        <h3>Linting/formatting setup</h3>
         <p>
           I've spend some time to define linting/formatting rules to have style consistency.
           The first one you definitely need in every project is <ExternalLink href="https://editorconfig.org">Editorconfig</ExternalLink>. Here is the one, I am using for my blog:
         </p>
         <Highlight className="INI" file=".editorconfig" title="Editorconfig configuration example">{editorconfig}</Highlight>
+        <h3>Syntax highlighting</h3>
+        <p>
+          When I started development I didn't think about code highlighting. I've made a few commits and started writing my first article. Actually, it's this one.
+          I reached linting/formatting chapter and needed to highlight for my `editorconfig`.
+        </p>
+        <p>
+          I've found the most popular highlighters libraries - <ExternalLink href="https://highlightjs.org/">Highlight.js</ExternalLink> and <ExternalLink href="https://prismjs.com/">Prismjs</ExternalLink>. Both are great and written in JS.
+          The next thing that came to my mind was "Are they compatible with React?".
+          Yes, they are. But there are two different ways how they can be used.
+        </p>
+        <p>
+          Prismjs has <ExternalLink href="https://github.com/tomchentw/react-prism">react-prism</ExternalLink> that might be helpful. Let's look how it's implemented.
+        </p>
+        <Highlight className="js" file="src/components/PrismCode.js" title="Prismjs source code">{prismjs}</Highlight>
+        <p>
+          But how `<b>Prism</b>` property happened to be here? The answer is so easy - react-prism depends on the existence of <b>globals.Prism </b>object.
+          Based on that, it requires to have both: `react-prism` and `prism` libraries the versions of which have to be managed separately. I didn't choose that way because I wanted something easier to maintain where I can simply put my library in `package.json` and use it.
+          There are a lot of workarounds how you can use Prismjs with React, here is one of them: "<ExternalLink href="https://betterstack.dev/blog/code-highlighting-in-react-using-prismjs/">Code highlighting in React using Prism.js</ExternalLink>"
+        </p>
+        <p>
+          It won't be fair if I don't share what I chose. <ExternalLink href="https://react-highlight.neostack.com/">React-highlight</ExternalLink> - React Component for Syntax Highlighting built on <ExternalLink href="https://highlightjs.org/">Highlight.js</ExternalLink>.
+          The main difference here is that you simply need to install one library. In addition, you can choose theme from <ExternalLink href="https://github.com/highlightjs/highlight.js/tree/master/src/styles">themes folder</ExternalLink>.
+        </p>
+        <h3>Deployment</h3>
+        <p>
+          It's time to make your first deployment. <ExternalLink href="https://zeit.co/">Zeit</ExternalLink> is the easiest way to deploy your websites.
+          There are <ExternalLink href="https://nextjs.now-examples.now.sh/">Next.js deploy examples</ExternalLink> that might have been chosen as a guide.
+          The most important thing I like in this tool is an easy deployment. You just need to link Zeit account to your Github (or another one) repository.
+        </p>
+
+        <h2>Conclusion</h2>
+        <p>
+          I hope it was useful to spend a few minutes by reading it. Remember that there are a lot of tools, but the only thing you should pay attention to is combining/managing them in a right way.
+        </p>
+        <p>
+          You always can read/follow blog code in <ExternalLink href="https://github.com/melnychukvitaliy/blog">Github</ExternalLink>.
+        </p>
+        <p>
+        </p>
       </div>
 
     </ArticlePage>
