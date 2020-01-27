416dbe5ef9e2162005667a05c13f2440fc54781a
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-01-27 12:26:50 +0200

    test simple example

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,5 +1,13 @@
 function Meta() {
-  return <div></div>;
+  return <div>
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@nytimesbits" />
+    <meta name="twitter:creator" content="@nickbilton" />
+    <meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
+    <meta property="og:title" content="A Twitter for My Sister" />
+    <meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
+    <meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
+  </div>;
 }
 
 export default Meta;
