9e388673334d604da96135850bec7a5317333595
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-02-09 11:49:06 +0200

    move scripts/links and analytic tracking into _app

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,5 @@
 import { ReactChild } from 'react';
 
-import Head from 'next/head';
-
 import Header from './Header';
 import Footer from './Footer';
 
@@ -10,25 +8,6 @@ interface IMetaProps {
 }
 export default ({ children }: IMetaProps) => (
   <div className="container">
-    <Head>
-      <link rel='shortcut icon' type='image/x-icon' href='/static/favicon.ico' />
-      <title>Vitaliy Melnychuk :: Personal Blog</title>
-      <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' />
-      <script dangerouslySetInnerHTML={{
-        __html: `
-        (function(f, a, t, h, o, m){
-          a[h]=a[h]||function(){
-            (a[h].q=a[h].q||[]).push(arguments)
-          };
-          o=f.createElement('script'),
-          m=f.getElementsByTagName('script')[0];
-          o.async=1; o.src=t; o.id='fathom-script';
-          m.parentNode.insertBefore(o,m)
-        })(document, window, '//vmelnychuk-blog-fathom.herokuapp.com/tracker.js', 'fathom');
-        fathom('set', 'siteId', 'CMMUR');
-        fathom('trackPageview');
-      `}} />
-    </Head>
     <Header />
     {children}
     <Footer />
---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import Router from 'next/router';
+import Head from 'next/head'
+
+import * as Fathom from 'fathom-client';
+
+import { AppProps } from 'next/app'
+
+// Record a pageview when route changes
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
+
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    // if (process.env.NODE_ENV === 'production') {
+    Fathom.load();
+    Fathom.setSiteId('ZFEWBXJZ');
+    Fathom.trackPageview();
+    // }
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Vitaliy Melnychuk :: Personal Blog</title>
+        <link rel='shortcut icon' type='image/x-icon' href='/static/favicon.ico' />
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
+}
