2d9c6ee4292560a055066dd88fa8a194f60a3885
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-03-12 18:27:27 +0200

    remove google site verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Head>
-        <meta name="google-site-verification" content="dkdJiOZg4vB9ojt3PWKNwFv6T2TZPLEC7reKWrOWiFQ" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Vitaliy Melnychuk :: Personal Blog</title>
         <link rel='shortcut icon' type='image/x-icon' href='/static/favicon.ico' />
