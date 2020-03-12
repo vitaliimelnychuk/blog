d0d741845df79802d9d64397db31b43943cfc147
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-03-12 19:02:01 +0200

    add infrastrcture overview first part

--- pages/articles/2020-03-12-deployment-as-code.tsx
+++ pages/articles/2020-03-12-deployment-as-code.tsx
@@ -8,7 +8,14 @@ const article = articles['2020-03-12-deployment-as-code']
 export default () => (
   <Layout >
     <ArticlePage {...article}>
-      <div></div>
+      <div>
+        <p>We've started from AWS, Google Cloud instances that were provisioned manually.
+        We don't even have dockerized applications. Now, we are deploying apps to Kubernetes cluster through Github and have deployment review along with deployment history.</p>
+
+        <h2>Traffic Path & Infrastructure overview</h2>
+        <p>There is no reason to describe what kind of domains we are using to handle all our traffic.
+        I am going to start from our main Nginx Proxy server and how traffic ends up at that point as the first hop.</p>
+      </div>
     </ArticlePage>
   </Layout >
 );
