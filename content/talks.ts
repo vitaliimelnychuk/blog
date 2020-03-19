5558f5aab860b2b702223a7752bf02ff1578fe11
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-03-19 12:46:49 +0200

    add "30s. How much doest it cost" talk

--- content/talks.ts
+++ content/talks.ts
@@ -13,6 +13,12 @@ export const talks = [
     url: "static/pdf/deployment-process-as-code.pdf"
   },
   {
+    title: "30ms. How much does it cost?",
+    description: "When we are working on the infrastructure especially on the network performance. We are facing with issue that is not clear from the first view. Here is my small talk how we've found network performance issue between our clusters that were hosted in different regions.",
+    date: new Date("14 Feb 2020"),
+    url: "static/pdf/30ms_how-much-does-it-cost.pdf"
+  },
+  {
     title: "Elasticsearch: Controlling relevance",
     description: "Over the last year, our team has been working to find and deliver relevant results. We use Elasticsearch, search in 15TB cluster and handle up to 20000 requests/sec. Here is my talk slides about how full text search works, what is relevant, and how to build your scoring function.",
     date: new Date("June 8 2019"),
