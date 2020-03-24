d669b1ec7ec68cdf0e930caa4b7c22df6151acc7
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-03-24 18:32:17 +0200

    add  black holes book detail page

---
+++ pages/books/black-holes-and-baby-universes-and-other-essays.tsx
@@ -0,0 +1,21 @@
+import { books } from '../../content/books'
+
+import Layout from '../../components/Layout'
+import MetaPreviewLink from '.././../components/Meta/PreviewLink'
+import BookPreview from '.././../components/Book/BookPreview'
+import BookQuotes from '.././../components/Book/BookQuotes'
+
+export default ({ }) => {
+  const book = books['black-holes-and-baby-universes-and-other-essays']
+
+  return (<Layout>
+    <div>
+      <MetaPreviewLink title={book.title} url={book.slug} image={book.img} />
+      <BookPreview {...book} showButtons={false} />
+      <BookQuotes quotes={[
+        'We live in the one specific story that has specific properties and details'
+      ]} />
+    </div>
+  </Layout>
+  )
+}
