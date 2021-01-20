4324efc6e577e7fd911715f85c7732cec5c0c134
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-01-20 19:21:28 +0000

    move strapi article

--- content/articles.ts
+++ content/articles.ts
@@ -5,11 +5,6 @@ export type Article = {
 }
 
 export default {
-  '2019-12-22-deploy-your-blog': {
-    title: 'How to deploy your own blog in 2019?',
-    date: new Date('22 Dec 2019'),
-    url: '/articles/2019-12-22-deploy-your-blog',
-  } as Article,
   '2020-08-28-build-a-website-with-gatsby-and-strapi': {
     title: 'Build a website with Gatsby and Strapi | Core concepts',
     date: new Date('28 July 2020'),
