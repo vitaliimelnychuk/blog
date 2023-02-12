af7b721da2bbd07ddb06635437b4c527665e36b3
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2023-02-12 19:34:02 +0000

    feat: update deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-import rehypePrism from '@mapbox/rehype-prism'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-    providerImportSource: '@mdx-js/react',
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
