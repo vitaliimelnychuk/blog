842155cb38e4ca7de2a1b7a4605c28784919dc16
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2024-04-28 15:51:20 +0100

    Update articles title

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -11,12 +11,12 @@ export default async function HomePage() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Writing on software design, company building, and the aerospace
-            industry.
+            Writing on software engineering, performance, and developer
+            experience tools.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            All of my long-form thoughts on programming, leadership, product
-            design, and more, collected in chronological order.
+            All of my long-form thoughts on programming and sometimes just
+            random stuff in chronological order.
           </p>
         </div>
       </Container>
